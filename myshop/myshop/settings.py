11504431c6e38e98fff01101e3e1329c943df1ea
Алексей <tihonovalexey29@gmail.com>
2017-07-24 21:31:57 +0300

    Первые два урока работают

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/shop/static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'shop', 'static', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
 
--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -19,16 +19,15 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    def get_asolute_url(self):
-        return reversed('store:ProductListByCategory', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('shop:ProductListByCategory', args=[self.slug])
 
 # Модель продукта
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', verbose_name="Категория")
     name = models.CharField(max_length=200, db_index=True, verbose_name="Название")
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='static/media/products/%Y/%m/%d/', blank=True, verbose_name="Изображение "
-                                                                                                    "товара")
+    image = models.ImageField(upload_to='products/', blank=True, verbose_name="Изображение товара")
     description = models.TextField(blank=True, verbose_name="Описание")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
     stock = models.PositiveIntegerField(verbose_name="На складе")
@@ -46,5 +45,4 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        print(reverse('shop:ProductDetail', args=[self.id, self.slug]))
         return reverse('shop:ProductDetail', args=[self.id, self.slug])
